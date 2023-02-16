b914ac49b98b49097d3db4e059b3654e0242fa07
Marco Silletti <msilletti@aliceposta.it>
2023-02-16 18:02:45 +0100

    crea gironi

--- greattools-app/src/app/crea-gironi/crea-gironi.component.ts
+++ greattools-app/src/app/crea-gironi/crea-gironi.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MatChipInputEvent } from '@angular/material/chips';
-import { zoomInDownAnimation } from 'angular-animations';
+import { shakeAnimation, shakeOnEnterAnimation, wobbleAnimation, wobbleOnEnterAnimation, zoomInDownOnEnterAnimation } from 'angular-animations';
 import { Girone } from '../shared/models/girone';
 
 @Component({
@@ -8,17 +8,19 @@ import { Girone } from '../shared/models/girone';
   templateUrl: './crea-gironi.component.html',
   styleUrls: ['./crea-gironi.component.scss'],
   animations: [
-    zoomInDownAnimation()
+    zoomInDownOnEnterAnimation({ delay: 100 }),
+    wobbleOnEnterAnimation({duration: 1000}),
+    shakeOnEnterAnimation({duration: 1000})
   ]
 })
 export class CreaGironiComponent {
   numeroGironi: number = 2;
-  elencosquadre:string[] = [];
+  elencosquadre: string[] = [];
   gironi: Girone[] = [];
   viewLoader: boolean = false;
   errorMessage: string[] = [];
   elaborazioneInCorso: boolean = false;
-
+  
   removeSquadra(nomesquadra: string) {
     const index = this.elencosquadre.indexOf(nomesquadra);
     if (index >= 0) {
@@ -38,12 +40,12 @@ export class CreaGironiComponent {
     event.chipInput!.clear();
   }
 
-  async creaGironi(): Promise<void>{
+  async creaGironi(): Promise<void> {
     this.elaborazioneInCorso = true;
     this.gironi = [];
     for (let i = 0; i < this.numeroGironi; i++) {
       var g: Girone = {
-        nome: 'Girone ' + (i+1),
+        nome: 'Girone ' + (i + 1),
         elencoSquadre: []
       };
       this.gironi.push(g);
@@ -52,24 +54,25 @@ export class CreaGironiComponent {
     var temp_s = this.elencosquadre;
     var n_gir = 0;
     while (temp_s.length > 0) {
+      this.viewLoader = true;
+      await this.delay(1000);
       this.viewLoader = false;
       var p = this.getRandomInt(this.elencosquadre.length);
       temp_s[p]
       this.gironi[n_gir].elencoSquadre.push(temp_s[p]);
       temp_s.splice(p, 1);
-
-      await this.delay(1000);
-      this.viewLoader = true;
-      n_gir ++;
-      if(n_gir >= this.numeroGironi) n_gir = 0;
-      await this.delay(1000);
+      n_gir++;
+      if (n_gir >= this.numeroGironi) n_gir = 0;
+      await this.delay(2000);
     }
     this.viewLoader = false;
+    this.elaborazioneInCorso = false;
   }
-  getRandomInt(max:number): number {
+  getRandomInt(max: number): number {
     return Math.floor(Math.random() * max);
   }
   delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
-}
+
